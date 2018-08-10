1d5f7def15289fd10b185a90aba71c9b9e18df26
Kudakwashe Paradzayi <kgparadzayi@gmail.com>
2018-08-10 16:35:56 +0200

    Add a nav bar to link the pages.
    -- its working on the dev machine
       I want to test it on netlify

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,12 @@
 <template>
   <div id="app">
-    <router-view/>
+    <nav class="nav">
+      <a href="/" class="brand">KayPay</a>
+      <div>
+        <router-link to="/transactions">Transactions</router-link>
+      </div>
+    </nav>
+    <router-view class="container"/>
   </div>
 </template>
 
@@ -13,5 +19,35 @@ export default {
 <style>
   body {
     font-family: 'Roboto', sans-serif;
+  }
+
+  .container, .nav {
+    width: 90%;
+    margin: 0 auto
+  }
+
+  .nav {
+    display: flex;
+    justify-content: space-between;
+    padding: 10px 0;
+    border-bottom: rgba(0, 0, 0, 0.03) solid 2px;
+  }
+
+  .brand {
+    font-weight: 900;
+    font-size: 20px;
+    /* letter-spacing: 5px; */
+  }
+
+  a {
+    text-decoration: none;
+    color: currentColor;
+    display: inline-block;
+    padding: 5px 10px;
+  }
+
+  a:hover {
+    background: rgba(0, 0, 0, 0.03);
+    border-radius: 4px;
   }
 </style>
